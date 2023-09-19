d3832f2c937fc798c27a8d6f7b3b3933e884f320
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2023-09-19 10:42:17 +0300

    Update config

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            href="/docs/intro">
+            href="/docs/docs1/intro">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
