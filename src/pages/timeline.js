c8bc8cb272e59d4954420fb0511b2b05f57dd377
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2025-06-05 14:56:30 +0300

    Modify timeline

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -8,26 +8,26 @@ const Timeline = () => {
             <div class="timeline-block timeline-block-right">
                 <div class="marker"></div>
                 <div class="timeline-content">
-                    <h4>Trainee FullStack Developer</h4>
+                    <h4>Trainee FullStack Developer la SCHWARZ</h4>
                     <p>Iunie 2025 - Prezent</p>
                     <p>
                         După multe interviuri, am reușit să prind un post la firma SCHWARZ IT România. Sunt mândru de mine,
                         întrucât au fost sute de candidați și doar 28 de interni acceptați, iar eu mă aflu printre ei. La această
                         firmă lucrez cu limbaje precum GO, dar și KOTLIN. Această experiență o să fie una interesantă, întrucât
-                        este prima oară când lucrez într-o corporație
+                        este prima oară când lucrez într-o corporație.
                     </p>
                 </div>
             </div>
             <div class="timeline-block timeline-block-left">
                 <div class="marker"></div>
                 <div class="timeline-content">
-                    <h4>Prof. la Asociația Învață pentru Tine</h4>
+                    <h4>Prof. la Asociația "Învață Pentru Tine"</h4>
                     <p>Iunie 2025 - Prezent</p>
                     <p>
                         Pasiunea pentru predat, informatică, tehnologie, dar și voluntariat m-au adus în postura de a alege să
                         devin voluntar la asociația prezentată mai sus. Iubesc să ajut oamenii, iar dacă pot face asta și voluntar,
                         de ce nu? Aici lucrez cu copii de toate vârstele, de la gimnaziu la liceu și îi ajut pe partea de
-                        Informatică & TIC, ce doresc ei să învețe
+                        Informatică & TIC, ce doresc ei să învețe.
                     </p>
                 </div>
             </div>
@@ -40,7 +40,7 @@ const Timeline = () => {
                         Predau Informatică & TIC la clasele IX-XI. Experiența de la un colegiu național este una unică pe care
                         oricând aș mai încerca-o. Este o adevărată provocare să le predai Informatică unor copii de liceu, mai
                         ales că aceștia nu au lucrat prea mult în gimnaziu, însă încerc să dau tot ce am mai bun pentru a-i
-                        învăța ceva util pe aceștia
+                        învăța ceva util pe aceștia.
                     </p>
                 </div>
             </div>
@@ -53,7 +53,7 @@ const Timeline = () => {
                         Algolymp reprezintă un centru de meditații pentru excelență, axat pe mai multe categorii de elevi: elevi
                         începători, elevi la nivel de clasă, elevi ce se pregătesc pentru examenul de bacalaureat, dar și elevi
                         ce doresc să participe la olimpiade județene, naționale sau chiar la lot. Fac parte din echipa aceasta de
-                        un bun timp și pot spune că reprezintă o provocare să trec de nivelul școlar și să fac performanță cu ei
+                        un bun timp și pot spune că reprezintă o provocare să trec de nivelul școlar și să fac performanță cu ei.
                     </p>
                 </div>
             </div>
@@ -66,34 +66,34 @@ const Timeline = () => {
                         Acest liceu este liceul meu de suflet, întrucât aici am învățat 4 ani. Predau totuși doar TIC, însă asta
                         nu mă oprește din a învăța alte lucruri noi și interesante. De când predau TIC, am învățat multe lucruri
                         utile pe care le poți face la această disciplină, de care nici eu nu știam. Colectivul este unul minunat,
-                        mă bucur să fiu coleg cu foștii mei profesori și să îi fac mândri de ceea ce am ajuns astăzi
+                        mă bucur să fiu coleg cu foștii mei profesori și să îi fac mândri de ceea ce am ajuns astăzi.
                     </p>
                 </div>
             </div>
             <div class="timeline-block timeline-block-left">
                 <div class="marker"></div>
                 <div class="timeline-content">
-                    <h4>Web Developer Intern</h4>
+                    <h4>Web Developer Intern la FeedCheck</h4>
                     <p>Iulie 2024 - Septembrie 2024</p>
                     <p>
                         Pot spune că FeedCheck, locul în care am ocupat această poziție, a fost primul meu Internship adevărat.
                         Aici am avut prezență fizică la muncă, la un birou. Chiar dacă era un StartUp, echipa a fost foarte
                         primitoare și deschisă, iar eu pot spune că am învățat lucruri noi. Din cauza facultății a trebuit să
                         renunț la poziție, cu toate că primisem contract de continuare, dar cine știe? Dumnezeu așează el
-                        lucrurile, iar acum am ajuns să fiu profesor
+                        lucrurile, iar acum am ajuns să fiu profesor.
                     </p>
                 </div>
             </div>
             <div class="timeline-block timeline-block-right">
                 <div class="marker"></div>
                 <div class="timeline-content">
-                    <h4>TeamLead & FrontEnd Developer</h4>
+                    <h4>TeamLead & FrontEnd Developer la DiviGO</h4>
                     <p>Mai 2024 - Octombrie 2024</p>
                     <p>
                         Pe lângă pasiunea mea pentru tehnologie se numără și partea de a "manageria" o echipă. La DiviGO, căci
                         așa se numea locul unde am lucrat, am avut ocazia să coordonez echipa de FrontEnd și să ajut totodată
                         și la dezvoltarea unei aplicații destul de interesantă. A fost o experiență frumoasă din care am mai
-                        învățat câte ceva și cu siguranță mi-ar plăcea să mai am astfel de experiențe și pe viitor
+                        învățat câte ceva și cu siguranță mi-ar plăcea să mai am astfel de experiențe și pe viitor.
                     </p>
                 </div>
             </div>
@@ -106,7 +106,7 @@ const Timeline = () => {
                         În calitate de asistent universitar în regim voluntar, am avut ocazia încă din anul 2 să ajut profesorii
                         și elevii în cadrul laboratoarelor. Până acum am făcut parte din echipa de voluntari la discipline
                         precum "Programarea Calculatoarelor și Limbaje de Programare", "Structuri de Date și Algoritmi", dar și
-                        "Tehnologii Web" și vreau să continui
+                        "Tehnologii Web" și vreau să continui.
                     </p>
                 </div>
             </div>
@@ -120,7 +120,7 @@ const Timeline = () => {
                         domeniu din România, anume la Facultatea de "Automatică și Calculatoare" din cadrul "Universității
                         Naționale de Științe și Tehnologie Politehnica București". Aici studiez în paralel și Modulul
                         Psihopedagogic I care mă vă ajuta să pot preda Informatica la ciclul V-X. Evident că pe viitor îmi doresc
-                        să urmez și studii de master
+                        să urmez și studii de master.
                     </p>
                 </div>
             </div>
@@ -134,7 +134,7 @@ const Timeline = () => {
                         Informatică. Această agendă a fost special creată pentru pregătirea pentru examenul de Bacalaureat și
                         conține toată materia aferentă acestuia. Am muncit o vară întreagă pentru acest produs, iar în zilele de
                         astăzi îi aducem îmbunătățiri pentru a fi la curent cu cerințele actuale. Această agendă poate fi găsită
-                        pe site-ul celor de la Edeea
+                        pe site-ul celor de la Edeea.
                     </p>
                 </div>
             </div>
@@ -148,7 +148,7 @@ const Timeline = () => {
                         scurt timp după asta, am zis să încerc să împărtășesc cunoștințele mele și cu alte persoane, în regim de
                         pregătiri individuale. Mi-a plăcut foarte mult să fac asta și iată că până în prezent mă ocup cu aceste
                         ședințe individuale de pregătire și inspir în continuare elevii să reușească să obțină note mari în
-                        examene
+                        examene.
                     </p>
                 </div>
             </div>
